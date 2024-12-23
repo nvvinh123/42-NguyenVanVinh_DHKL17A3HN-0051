db5ec6173106ef9710446cfc2bff08aeac35a56a
42-nguyen van vinh_23174600051_DHKL17a3hn <148510061+nvvinh123@users.noreply.github.com>
2024-12-24 00:17:12 +0700

    Add files via upload

---
+++ BT_CHUONG_4/cau18.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import re
+
+emails = pd.Series(['buying books at amazom.com', 'rameses@egypt.com', 'matt@t.co', 'narendra@modi.com'])
+
+pattern ='[A-Za-z0-9.%+-]+@[A-Za-z0-9.-]+.[A-Za-z]{2,4}'
+
+matches = emails.str.extract(pattern)
+
+print(matches)
